871505bbf9cd965bdcdbe6f966bf5367c036031f
Juan Charris <Juan.Charris2110@gmail.com>
2025-02-14 18:36:28 -0500

    xd

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from app.routes.ciego_routes import router as ciego_router
 from app.routes.roles_routes import router as roles_router
 from app.routes.tipo_mascota_routes import router as tipo_mascota_router
 from app.routes.genero_mascota_routes import router as genero_mascota_router
-from app.routes.collares_gps_routes import router as collares_gps_router
+from app.routes.gps_routes import router as gps_router
 from app.routes.admin_routes import router as admin_router
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -27,7 +27,7 @@ app.include_router(ciego_router)
 app.include_router(roles_router)
 app.include_router(tipo_mascota_router)
 app.include_router(genero_mascota_router)
-app.include_router(collares_gps_router)
+app.include_router(gps_router)
 app.include_router(admin_router)
 
 

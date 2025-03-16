da44ec5f64c13d33e4c277d1973fac8657d8e70d
Juan Charris <Juan.Charris2110@gmail.com>
2025-03-16 14:43:27 -0500

    se coloco     allow_origins=["https://sensutrack-front.onrender.com"],

--- app/main.py
+++ app/main.py
@@ -12,9 +12,17 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+#app.add_middleware(
+#    CORSMiddleware,
+#    allow_origins=["*"],
+#    allow_credentials=True,
+#    allow_methods=["*"],
+#    allow_headers=["*"],
+#)
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://sensutrack-front.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

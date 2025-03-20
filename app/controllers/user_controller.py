afea49115e519ca74b678c14358bd81773dd6f5b
Juan Charris <Juan.Charris2110@gmail.com>
2025-03-19 22:32:45 -0500

    xd

--- app/controllers/user_controller.py
+++ app/controllers/user_controller.py
@@ -430,8 +430,8 @@ class Usercontroller():
             conn = get_db_connection()
             cursor = conn.cursor()
             cursor.execute(
-                "UPDATE usuarios SET email = %s, nombre = %s, apellido = %s, documento = %s, telefono = %s, estado = %s WHERE id = %s",
-                (user.email, user.nombre, user.apellido,
+                "UPDATE usuarios SET email = %s, password = %s, nombre = %s, apellido = %s, documento = %s, telefono = %s, estado = %s WHERE id = %s",
+                (user.email, user.password, user.nombre, user.apellido,
                  user.documento, user.telefono, user.estado, user_id,)
             )
             conn.commit()
